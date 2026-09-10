9e65f9f3b64ea31d48f330dbaad9b61b9e0983b8
Zenluo922 <2339893571@qq.com>
2026-09-11 00:14:06 +0800

    Delete Demo directory

--- Demo/Demo1Constant.java
+++
@@ -1,6 +0,0 @@
-public class Demo1Constant{
-	public static void main(String[] args){
-		System.out.println(1);
-		System.out.println(-1);
-	}
-}
--- Demo/Demo4Var.java
+++
@@ -1,6 +0,0 @@
-public class Demo4Var{
-	public static void main(String[] args){
-		String path = "C:\Windows\System32\cmd.exe";
-		System.out.println(path);
-	}
-}
